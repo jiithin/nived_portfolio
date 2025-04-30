4b787ad88c3ee88c5071b820255b8a14f5183a48
jiithin <jithijithin9@gmail.com>
2025-04-30 20:11:48 +0530

    footer added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className="font-Leto h-auto flex flex-col justify-center bg-neutral-900 lg:pt-20 pt-32 lg:px-20 pb-20 
+    ">
+      <h1 className="text-4xl font-semibold text-white text-center pb-5">Contact Me (Add later)</h1>
+      </div>
   )
 }
 
