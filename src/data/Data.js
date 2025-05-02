1369611431b0455fddbeaf7171fd2720c91db623
jiithin <jithijithin9@gmail.com>
2025-05-02 17:00:57 +0530

    -

--- src/data/Data.js
+++ src/data/Data.js
@@ -1,18 +1,18 @@
 export const ProjectList = [
     {
-      imge: "/realestate.png",
+      imge: "/realestate.webp",
       title: "REAL ESTATE WEBSITE",
       type:"UI&UX",
       demo_url: "https://www.behance.net/gallery/220856039/real-estate-website",
     },
     {
-        imge: "/bento.png",
+        imge: "/bento.webp",
         title: "Bento Grid",
         type:"BRANDING",
         demo_url: "https://www.behance.net/gallery/224615257/Bento-Gride-design",
       },
       {
-        imge: "/earth.png",
+        imge: "/earth.webp",
         title: "Earth Poster",
         type:"GRAPHIC WORK",
         demo_url: "https://www.behance.net/gallery/219561119/Earth-day-poster",
